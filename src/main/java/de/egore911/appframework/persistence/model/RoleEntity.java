358069f6fb8f54945088d24f126a3085589f05cb
Christoph Brill <egore911@gmail.com>
2016-12-27 12:25:55 +0100

    Drop unused imports

--- src/main/java/de/egore911/appframework/persistence/model/RoleEntity.java
+++ src/main/java/de/egore911/appframework/persistence/model/RoleEntity.java
@@ -28,8 +28,6 @@ import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OrderBy;
