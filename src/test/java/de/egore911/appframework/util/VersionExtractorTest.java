d6c4ef8791d2fb1fb200d6954e7f9bb4657ed105
Christoph Brill <egore911@gmail.com>
2016-12-27 12:23:14 +0100

    Add tests from capacity-web

---
+++ src/test/java/de/egore911/appframework/util/VersionExtractorTest.java
@@ -0,0 +1,43 @@
+package de.egore911.appframework.util;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletContext;
+
+import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+public class VersionExtractorTest {
+
+	private ServletContext mockContext() {
+		ServletContext context = Mockito.mock(ServletContext.class);
+		Mockito.when(context.getResourceAsStream(Matchers.anyString())).thenAnswer(invocation -> VersionExtractorTest.class.getResourceAsStream((String) invocation.getArguments()[0]));
+		return context;
+	}
+
+	@Test
+	public void testGetMavenVersion() {
+		ServletContext context = mockContext();
+		String mavenVersion = VersionExtractor.getMavenVersion(context, "de.egore911.capacity", "capacity-test");
+		assertThat(mavenVersion, equalTo("0.0.1-SNAPSHOT"));
+	}
+
+	@Test
+	public void testGetGitRevision() {
+		ServletContext context = mockContext();
+		String mavenVersion = VersionExtractor.getGitVersion(context, "de.egore911.capacity", "capacity-test");
+		assertThat(mavenVersion, equalTo("fe4d7ae"));
+	}
+
+	@Test
+	public void testGetBuildTimestamp() {
+		ServletContext context = mockContext();
+		LocalDateTime buildTimestamp = VersionExtractor.getBuildTimestamp(context, "de.egore911.capacity", "capacity-test");
+		assertThat(buildTimestamp, equalTo(LocalDateTime.of(2016, 4, 26, 11, 20, 44)));
+	}
+
+}
