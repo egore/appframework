7f858712f0af3b48114c9dafab78d8bde14b6688
Christoph Brill <egore911@gmail.com>
2016-12-30 11:59:33 +0100

    Drop VersionService and add missing User<->UserEntity mapping

--- src/main/java/de/egore911/appframework/util/FactoryHolder.java
+++ src/main/java/de/egore911/appframework/util/FactoryHolder.java
@@ -7,8 +7,17 @@ import java.time.LocalTime;
 import java.time.ZonedDateTime;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections4.CollectionUtils;
+
+import de.egore911.appframework.persistence.dao.RoleDao;
+import de.egore911.appframework.persistence.model.RoleEntity;
+import de.egore911.appframework.persistence.model.UserEntity;
+import de.egore911.appframework.ui.dto.User;
+import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.converter.BidirectionalConverter;
 import ma.glasnost.orika.converter.builtin.PassThroughConverter;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
@@ -46,5 +55,22 @@ public class FactoryHolder {
 					}
 
 				});
+
+		FactoryHolder.MAPPER_FACTORY.classMap(User.class, UserEntity.class).byDefault()
+		.customize(new CustomMapper<User, UserEntity>() {
+			@Override
+			public void mapAtoB(User a, UserEntity b, MappingContext context) {
+				if (CollectionUtils.isNotEmpty(a.getRoleIds())) {
+					b.setRoles(new RoleDao().findByIds(a.getRoleIds()));
+				}
+			}
+
+			@Override
+			public void mapBtoA(UserEntity a, User b, MappingContext context) {
+				b.setPassword(null);
+				b.setRoleIds(a.getRoles().stream().map(RoleEntity::getId).collect(Collectors.toList()));
+			}
+		}).register();
+
 	}
 }
