5c801ccf0ea0bc4927a9d5d86342927aafa1308e
Christoph Brill <egore911@gmail.com>
2017-01-06 10:57:13 +0100

    Make class publically available

--- src/main/java/de/egore911/appframework/ui/rest/AbstractService.java
+++ src/main/java/de/egore911/appframework/ui/rest/AbstractService.java
@@ -3,7 +3,7 @@ package de.egore911.appframework.ui.rest;
 import de.egore911.appframework.util.FactoryHolder;
 import ma.glasnost.orika.MapperFacade;
 
-abstract class AbstractService {
+public abstract class AbstractService {
 
 	protected final MapperFacade getMapper() {
 		return FactoryHolder.MAPPER_FACTORY.getMapperFacade();
