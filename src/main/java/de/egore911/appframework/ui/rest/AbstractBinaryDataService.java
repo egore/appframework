6280176040b8c80e54ab0e9a717f4013e91ac3a0
Christoph Brill <christoph.brill@cgm.com>
2018-01-30 00:15:50 +0100

    Implement content disposition for binary data downloads

--- src/main/java/de/egore911/appframework/ui/rest/AbstractBinaryDataService.java
+++ src/main/java/de/egore911/appframework/ui/rest/AbstractBinaryDataService.java
@@ -1,60 +1,75 @@
 package de.egore911.appframework.ui.rest;
 
-import java.util.List;
-
-import javax.annotation.Nonnull;
-import javax.persistence.EntityManager;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import de.egore911.appframework.persistence.dao.BinaryDataDao;
 import de.egore911.appframework.persistence.model.BinaryDataEntity;
 import de.egore911.appframework.persistence.model.UserEntity;
 import de.egore911.appframework.persistence.selector.UserSelector;
 import de.egore911.persistence.util.EntityManagerUtil;
+import org.glassfish.jersey.media.multipart.ContentDisposition;
+
+import javax.annotation.Nonnull;
+import javax.persistence.EntityManager;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.StreamingOutput;
+import java.sql.Date;
+import java.time.ZoneId;
+import java.util.List;
 
 public abstract class AbstractBinaryDataService extends AbstractService {
 
-	@GET
-	@Path("{id}")
-	@Produces(MediaType.APPLICATION_OCTET_STREAM)
-	public byte[] getRaw(@PathParam("id") Integer id) {
-		BinaryDataEntity binaryData = new BinaryDataDao().findById(id);
-		return binaryData.getData();
-	}
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
+    public Response getRaw(@PathParam("id") Integer id) {
+        BinaryDataEntity binaryData = new BinaryDataDao().findById(id);
 
-	@DELETE
-	@Path("/{id}")
-	public void delete(@PathParam("id") Integer id) {
-		EntityManager em = EntityManagerUtil.getEntityManager();
-		try {
-			em.getTransaction().begin();
-			nullAndPersistOwnUsers(em, id);
-			nullAndPersistUsers(em, id);
-			BinaryDataDao binaryDataDao = new BinaryDataDao();
-			BinaryDataEntity binaryData = binaryDataDao.findById(id);
-			binaryDataDao.remove(binaryData);
-			em.getTransaction().commit();
-		} finally {
-			if (em.getTransaction().isActive()) {
-				em.getTransaction().rollback();
-			}
-		}
-	}
-	
-	private void nullAndPersistOwnUsers(@Nonnull EntityManager em, @Nonnull Integer id) {
-		List<UserEntity> users = new UserSelector().withPictureId(id)
-				.findAll();
-		users.forEach(user -> {
-			user.setPicture(null);
-			em.persist(user);
-		});
-	}
+        if (binaryData == null) {
+            throw new NotFoundException("No binary data with given ID");
+        }
 
-	protected abstract void nullAndPersistUsers(@Nonnull EntityManager em, @Nonnull Integer id);
+        ContentDisposition contentDisposition = ContentDisposition
+                .type("attachment")
+                .fileName(binaryData.getFilename())
+                .size(binaryData.getSize())
+                .creationDate(Date.from(binaryData.getCreated().atZone(ZoneId.systemDefault()).toInstant())).build();
+
+        return Response
+                .ok((StreamingOutput) outputStream -> outputStream.write(binaryData.getData()))
+                .header("Content-Disposition", contentDisposition)
+                .build();
+
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void delete(@PathParam("id") Integer id) {
+        EntityManager em = EntityManagerUtil.getEntityManager();
+        try {
+            em.getTransaction().begin();
+            nullAndPersistOwnUsers(em, id);
+            nullAndPersistUsers(em, id);
+            BinaryDataDao binaryDataDao = new BinaryDataDao();
+            BinaryDataEntity binaryData = binaryDataDao.findById(id);
+            binaryDataDao.remove(binaryData);
+            em.getTransaction().commit();
+        } finally {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+        }
+    }
+
+    private void nullAndPersistOwnUsers(@Nonnull EntityManager em, @Nonnull Integer id) {
+        List<UserEntity> users = new UserSelector().withPictureId(id)
+                .findAll();
+        users.forEach(user -> {
+            user.setPicture(null);
+            em.persist(user);
+        });
+    }
+
+    protected abstract void nullAndPersistUsers(@Nonnull EntityManager em, @Nonnull Integer id);
 
 }
